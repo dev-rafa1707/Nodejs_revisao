7dd7b277f233f34c5057e3ac0fd1228615c56a0d
dev-rafa1707 <rafa1707@gmail.com>
2023-04-10 20:56:03 -0300

    configura primeira rota get all em '/'

--- routes.js
+++ routes.js
@@ -1,5 +1,14 @@
 const express = require("express")
 const routes = express.Router()
 
+let lista = [{"id":1,"nome":"Joao", "idade":18},
+             {"id":2,"nome":"Maria", "idade":35},
+             {"id":3,"nome":"Paula", "idade":27},
+            {"id":4,"nome":"Fabiano", "idade":50},]
+
+
+routes.get("/",(req,res)=>{
+    res.status(200).json(lista)
+})
 
 module.exports = routes
